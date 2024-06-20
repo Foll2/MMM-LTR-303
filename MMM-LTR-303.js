54e1423a0e7951956ae4639632090b4f3a272367
Foll2 <73439480+Foll2@users.noreply.github.com>
2024-06-20 14:56:18 +0200

    Fixed issue if current Lux was more then maxLux

--- MMM-LTR-303.js
+++ MMM-LTR-303.js
@@ -49,14 +49,14 @@ Module.register("MMM-LTR-303Dimming", {
     },
 
     updateOverlay: function(payload) {
-        this.opacity = this.config.maxDim - (payload / this.config.maxLux) * this.config.maxDim;
+        this.opacity = Math.max(0, this.config.maxDim - (payload / this.config.maxLux) * this.config.maxDim);
         // 0lux = 0.9dim
         // 500lux = 0dim
 
         this.overlay.style.transitionDuration = '800ms';
         this.overlay.style.backgroundColor = `rgba(0, 0, 0, ${this.opacity})`;
 
-        this.updateDom(800);
+        this.updateDom(1000);
     },
 
     getDom: function() {
